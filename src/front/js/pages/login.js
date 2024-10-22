f47ff695b748f8a37d5e6be86f8093737fbbc4a5
Sara uri <146222294+Suri1336@users.noreply.github.com>
2024-10-22 22:48:12 +0000

    done

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -5,33 +5,36 @@ import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 export const Login = () => {
-	const { store, actions } = useContext(Context);
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
-    const navigate=useNavigate();
-    const handleLogin=(e)=>{
+    const { store, actions } = useContext(Context);
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate();
+    const handleLogin = (e) => {
         e.preventDefault();
-        actions.login(email,password)
+        actions.login(email, password)
     };
-    if (store.token&&store.token!==""&&store.token!==undefined){
-        navigate("/private")
-    }
+    useEffect(() => {
+         if (store.token && store.token !== "" && store.token !== undefined) {
+            navigate("/private")
+        }
+    }, [store.token, navigate])
+   
     return (
         <div>
             <div className="container">
-                {(store.token&&store.token!==""&&store.token!==undefined)?
-                "you are loged in with token"+store.token
-            
-            :
-           
-                <>
-                <input type="text" placeholder="email" onChange={e => setEmail(e.target.value)} value={email} />
-                <input type="password" placeholder="password" onChange={e => setPassword(e.target.value)} value={password} />
-                <button onClick={handleLogin}>Login</button>
-                </>
+                {(store.token && store.token !== "" && store.token !== undefined) ?
+                    "you are loged in with token" + store.token
 
-                
-            }
+                    :
+
+                    <>
+                        <input type="text" placeholder="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+                        <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                        <button onClick={handleLogin}>Login</button>
+                    </>
+
+
+                }
 
             </div>
         </div>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,7 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			token:null,
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -23,14 +23,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
@@ -49,74 +49,74 @@ const getState = ({ getStore, getActions, setStore }) => {
 				//reset the global store
 				setStore({ demo: demo });
 			},
-			syncSessionToken:()=>{
-				const token=sessionStorage.getItem('token');
-				if (token&&token!==""&&token!==undefined){
-					setStore({token:token})
+			syncSessionToken: () => {
+				const token = sessionStorage.getItem('token');
+				if (token && token !== "" && token !== undefined) {
+					setStore({ token: token })
 				}
 			},
-			login:async(email,password)=>{
-				const options={
-					method:"POST",
-					headers:{
-						"Content-Type":"application/json"
+			login: async (email, password) => {
+				const options = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
 					},
-					body:JSON.stringify(
+					body: JSON.stringify(
 						{
-							email:email,
-							password:password
+							email: email,
+							password: password
 						}
 					)
 				}
-				try{
-					const response=await fetch(process.env.BACKEND_URL+"/api/token",options)
-					if (response.status!==200){
+				try {
+					const response = await fetch(process.env.BACKEND_URL + "/api/token", options)
+					if (response.status !== 200) {
 						alert("Error response code", response.status)
 						return false;
 					}
-					const data=await response.json()
-					console.log("access token",data);
-					sessionStorage.setItem("token",data.access_token);
+					const data = await response.json()
+					console.log("access token", data);
+					sessionStorage.setItem("token", data.access_token);
 					setStore({
-						token:data.access_token
+						token: data.access_token
 					})
 					return true;
 				}
-				catch(error){
+				catch (error) {
 					console.log("login error please try again")
 				}
 			},
-			logout:()=>{
+			logout: () => {
 				sessionStorage.removeItem("token");
 				console.log("you are logged out")
-				setStore ({
-					token:null
+				setStore({
+					token: null
 				})
 			},
-			addUser:async(email,password)=>{
-				const options={
-					method:"POST",
-					headers:{
-						"Content-Type":"application/json"
+			addUser: async (email, password) => {
+				const options = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
 					},
-					body:JSON.stringify(
+					body: JSON.stringify(
 						{
-							email:email,
-							password:password
+							email: email,
+							password: password
 						}
 					)
 				}
-				try{
-					const response=await fetch(process.env.BACKED_URL+"api/signup",options)
-					if(response.status!==200){
-						alert("error response code",response.status)
+				try {
+					const response = await fetch(process.env.BACKEND_URL + "/api/signup", options)
+					if (response.status !== 200) {
+						alert("error response code", response.status)
 						return false
 					}
-					const data=await response.json()
-					console.log("from the backend",data)
+					const data = await response.json()
+					console.log("from the backend", data)
 					return true
 				}
-				catch(error){console.log("login error")}
+				catch (error) { console.log("login error") }
 
 			}
 		}
